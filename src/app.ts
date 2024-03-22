01d3e4e0abf500a69ca6e6aafeb16d4b5bacf272
ehandresen <eirik.horgen.andresen@gmail.com>
2024-03-22 20:33:19 +0100

    added models, routes & controllers folders

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import notesRoutes from './routes/notes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.json());
+
+app.use('/api/notes', notesRoutes);
+
+app.use((req, res, next) => {
+  next(Error('Endpoint not found'));
+});
+
+// Error Handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+  let errorMessage = 'An unknown error occurred';
+
+  if (error instanceof Error) errorMessage = error.message; // Every error has a 'message' property
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
