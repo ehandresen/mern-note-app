19c0130f2ffaa06ed2b96e8d4b5b93a2a44c2724
ehandresen <eirik.horgen.andresen@gmail.com>
2024-03-22 13:14:22 +0100

    added mongoose & dotenv package

--- src/server.ts
+++ src/server.ts
@@ -10,3 +10,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Running on port: ${port}`);
 });
+
+// mongoDBpass: dsoqNHjIXP7ueiTh
+// mongodb+srv://ehandresen:dsoqNHjIXP7ueiTh@cluster0.mhoiteg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// mongodb+srv://ehandresen:<password>@cluster0.mhoiteg.mongodb.net/
