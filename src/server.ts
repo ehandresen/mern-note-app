0f243e8f14c110d72d2c2c3703269ff7ee025f83
ehandresen <eirik.horgen.andresen@gmail.com>
2024-03-22 12:13:51 +0100

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hellooooo Wooooorld!');
+});
+
+app.listen(port, () => {
+  console.log(`Running on port: ${port}`);
+});
